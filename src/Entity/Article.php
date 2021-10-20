1af41900c694b689e34e36f2fc0de187ab42b60b
boutouditsaid@gmail.com <boutouditsaid@gmail.com>
2021-10-20 23:51:24 +0100

    first commit

---
+++ src/Entity/Article.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+
+/**
+ * @ApiResource(
+ *  normalizationContext={"groups"={"article:read"}},
+ *  denormalizationContext={"groups"={"article:write"}}
+ * )
+ * @ORM\Entity(repositoryClass=ArticleRepository::class)
+ */
+class Article
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     * 
+     * @Groups({"article:read"})
+     */
+    private $id;
+    
+    /**
+     * @ORM\Column(type="string",length=255)
+     * 
+     * @Groups({"article:read", "article:write"})
+     */
+    private $title;
+
+
+    /**
+     * @ORM\Column(type="string",length=255)
+     * 
+     * @Groups({"article:read"})
+     */
+    private $slug;
+
+
+    /**
+     * @ORM\Column(type="text",length=255, nullable=true)
+     * 
+     * @Groups({"article:read", "article:write"})
+     */
+    private $content;
+
+
+    /**
+     * @ORM\Column(type="string",length=255, nullable=true)
+     * 
+     * @Groups({"article:read", "article:write"})
+     */
+    private $picture;
+
+
+    /**
+     * @ORM\Column(type="boolean")
+     * 
+     * @Groups({"article:read"})
+     */
+    private $isPublished;
+
+
+    /**
+     * @ORM\Column(type="datetime")
+     * 
+     * @Groups({"article:read"})
+     */
+    private $publishedAt;
+
+
+    /**
+     * @ORM\Column(type="datetime")
+     * 
+     *  @Groups({"article:read"})
+     */
+    private $updatedAt;
+    
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+
+    public function getTitle()
+    {
+    return $this->title;
+    }
+
+    public function setTitle($title)
+    {
+    $this->title = $title;
+
+    return $this;
+    }
+
+    
+    public function getSlug()
+    {
+    return $this->slug;
+    }
+
+    public function setSlug($slug)
+    {
+    $this->slug = $slug;
+
+    return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(?string $picture): self
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+
+    public function getIsPublished(): ?bool
+    {
+        return $this->isPublished;
+    }
+
+    public function setIsPublished(bool $isPublished): self
+    {
+        $this->isPublished = $isPublished;
+
+        return $this;
+    }
+
+    public function getPublishedAt(): ?\DateTimeInterface
+    {
+        return $this->publishedAt;
+    }
+
+    public function setPublishedAt(\DateTimeInterface $publishedAt): self
+    {
+        $this->publishedAt = $publishedAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+
+
+
+
+
+}
